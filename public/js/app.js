51fbbc66bb411f91dbc13318b26dbaf6c5c143a8
Robert Hanley <robhanleyinteractive@Roberts-MacBook-Pro.local>
2012-04-21 18:32:53 -0400

    Example Namespace

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+var Exo = (function (obj) {
+	
+	_this = obj;
+
+	_this.global = function() {
+		//global function
+	}
+
+	function private() {
+		//private function
+	}
+	
+	return my;
+}(Exo || {}));
