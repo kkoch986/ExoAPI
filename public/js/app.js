966295be4d93e0390a6fc908490d2a2781346790
Robert Hanley <robhanleyinteractive@Roberts-MacBook-Pro.local>
2012-04-21 20:55:21 -0400

    updated layout stuff

--- public/js/app.js
+++ public/js/app.js
@@ -9,6 +9,29 @@ var Exo = (function (obj) {
 	function private() {
 		//private function
 	}
+
+	function initRollovers() {
+		$('li.roll-trigger').mouseenter(function() {
+			$(this).find('div.back-anim').show();
+			$(this).find('div.back-anim').stop(true, true).animate({height:'45px'}, 100);
+			$(this).find('h3').css({color: '#FFF'});
+		});
+
+		$('li.roll-trigger').mouseleave(function() {
+			$(this).find('div.back-anim').stop(true, true).animate({height:'0px'}, 100, 
+				function() {
+					$(this).hide();
+
+			});
+		});
+	}
+
+	_this.initGlobalApp = function() {
+		initRollovers();
+	}
+
+
 	
-	return my;
+	return obj;
+
 }(Exo || {}));
