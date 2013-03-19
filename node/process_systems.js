5257518f7a683c55c4359551360091e074398108
Ken <kkoch986@gmail.com>
2013-03-19 17:56:04 -0400

    updated the new pull script

--- node/process_systems.js
+++ node/process_systems.js
@@ -14,7 +14,7 @@ function pullUpdate()
 	    console.log('exec error: ' + error);
 	  }
 
-	  console.log("Update Complete.");
+	  console.log("[" + new Date() + "] Update Complete.");
 	  // now connect the database
 	  connectDB();
 
@@ -26,6 +26,7 @@ var mongodb = require("mongodb"),
 		//mongoserver = new mongodb.Server("172.16.3.30", 27017),
 		db_connector = new mongodb.Db("v3", mongoserver, {safe:false});
 
+var _db;
 function connectDB()
 {
 	/* Connect to Mongo */
@@ -38,6 +39,7 @@ function openCollection(err, db)
 {
 	if(err != undefined)
 		console.log(err);
+	_db = db;
 	db.collection("systems", startParsing);
 }
 
@@ -77,8 +79,66 @@ function startParsing(err, collection)
 		collection.save(object);
 	}
 
-	console.log("[" + new Date() + "] Complete Update ("+x+") Records Processed");
-	db_connector.close();
+	// Now process the CSV file
+	var text = fs.readFileSync("../exo_data/data.csv", "UTF-8");
+	var lines = text.split("\n");
+
+	// the first line will be the fields for each successive line
+	var fields = lines[0].split(",");
+	for(var x = 0 ; x < fields.length ; x++)
+	{
+		var prefix = fields[x].substr(0, 2);
+		var name = fields[x].substr(3, fields[x].length).replace(/\([a-zA-Z0-9].*\)/, "").trim().split(".").join("").split(" ").join("_");
+		fields[x] = (prefix + "_" + name).toLowerCase();
+		
+		// also handle some special cases here i.e. [fe/h]
+		if(fields[x] == "s._[fe/h]") fields[x] = "s._metallicity";
+	}
+
+	console.log("[" + new Date() + "] Complete Systems Update ("+count+") Records Processed");
+
+	_db.collection("systems", function(err, collection){
+		count = 0;
+		for(var x = 1; x < lines.length; x++)
+		{
+			var values = lines[x].split(",");
+			// construct an object out of each index in the fields array
+			if(values.length != fields.length) continue ;
+
+			var object = {"star":{}};
+			for(var y = 0; y < values.length; y++)
+			{
+				if(fields[y].substr(0, 3) == "s._")
+				{
+					var fname = fields[y].substr(3, fields[y].length);
+					if(isNumber(values[y])) values[y] = Number(values[y]);
+					object.star[fname] = values[y];
+				}
+				else if(fields[y].substr(0, 3) == "p._")
+				{
+					var fname = fields[y].substr(3, fields[y].length);
+					if(isNumber(values[y])) values[y] = Number(values[y]);
+					object[fname] = values[y];
+				}
+				else
+					object[fields[y]] = values[y];
+			}
+
+			object._id = object.name;
+			collection.save(object);
+			count++;
+		}
+
+		console.log("[" + new Date() + "] Complete Additional Planet Update ("+count+") Records Processed");
+		db_connector.close();
+
+		console.log("[" + new Date() + "] Running System/Planet Extractor.");
+		exec("mongo localhost:12002/v3 transpose.js", function (error, stdout, stderr) {
+			if (error !== null)
+				console.log('exec error: ' + error);
+			console.log("[" + new Date() + "] System/Planet Extractor Complete.");
+		});
+	});
 }
 
 /** a recursive function for parsing an xml element into a json object. */
