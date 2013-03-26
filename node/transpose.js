aed405b5eff7b0798097e6424974a0f5b7ad1a0f
Ken <kkoch986@gmail.com>
2013-03-26 15:36:23 -0400

    Added an extractor for stars from systems

--- node/transpose.js
+++ node/transpose.js
@@ -1,5 +1,4 @@
 // MONGODB CODE TO EXTRACT PLANETS FROM THE SYSTEMS TABLE INTO A PLANETS TABLE
-// Work in progress...
 
 // db.systems.find( { "star.planet": { $exists: true }  }, { "star.planet":1, "_id":0 } ).length()
 // db.systems.find( { "binary.star.planet": { $exists: true }  }, { "binary.star.planet":1, "_id":0 } ).length()
@@ -68,6 +67,74 @@ db.systems.find( { "binary.star.planet": { $exists: true }  }, { "binary.star.pl
 // currently this doesnt happen, there are binary.binary.star but none have planets
 // db.systems.find( { "binary.binary.star.planet": { $exists: true }  }, { "binary.binary.star.planet":1, "_id":0 } ).forEach( function(x){ mapPlanetFunction(x.binary.binary.star.planet); } );
 
+// MONGODB CODE TO EXTRACT STARS FROM THE SYSTEMS TABLE INTO A STARS TABLE
+
+var mapStarFunction = function(star) { 
+		if(star instanceof Array)
+		{
+			for (var i = star.length - 1; i >= 0; i--) {
+				mapStarFunction(star[i]);
+			};
+			return ;
+		}
+
+		if(star == null || star.name == null)
+			return; 
+
+		star._id = (star.name instanceof Array) ? star.name[0] : star.name; 
+
+		db.stars.save(star); 
+	}
+
+// get the basic system.star case
+db.systems.find( { "star": { $exists: true }  }, { "star":1, "_id":0 } ).forEach( function(x){ 
+	mapStarFunction(x.star); 
+} );
+
+// get the binary.star case
+db.systems.find( { "binary.star": { $exists: true }  }, { "binary.star":1, "_id":0 } ).forEach( function(x){ 
+	if(x.binary instanceof Array) {
+		for (var i = x.binary.length - 1; i >= 0; i--) {
+			mapStarFunction(x.binary[i].star);
+		};
+	}
+	else
+		mapStarFunction(x.binary.star); 
+} );
+
+// get the binary.binary.star case
+db.systems.find( { "binary.binary.star": { $exists: true }  }, { "binary.binary.star":1, "_id":0 } ).forEach( function(x){ 
+	if(x.binary instanceof Array)
+	{
+		for (var i = x.binary.length - 1; i >= 0; i--) {
+			if(x.binary[i].binary instanceof Array)
+			{
+				for (var j = x.binary[j].binary.length - 1; j >= 0; j--) {
+					mapStarFunction(x.binary[i].binary[j].star);
+				};
+			}
+			else
+			{
+				mapStarFunction(x.binary[i].binary.planet); 
+			}
+		};
+	}
+	else
+	{
+		if(x.binary.binary instanceof Array)
+		{
+			for (var j = x.binary.binary.length - 1; j >= 0; j--) {
+					mapStarFunction(x.binary.binary[j].star);
+			};
+		}
+		else
+		{
+			mapStarFunction(x.binary.binary.star); 
+		}
+	}
+});
+
+///////// scratchwork queries using map reduce ////////////////
 // //// Compute the Average mass per planet on each planet list (note: doesnt return correctly for lists or kepler list planets)
 // var mapFunction = function() { 
 // 		if(this.mass != 0)
